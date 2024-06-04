0d2669638438a8a910e577ea33b0c8af5053e0b9
Lucie <lye10@hawk.iit.edu>
2024-06-04 02:03:56 -0500

    Added dashboard revenue component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function DashboardPage() {
-    return <p>Dashboard</p>
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { lusitana } from '../ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+
+export default async function DashboardPage() {
+    const revenue = await fetchRevenue();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+            <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'></div>
+            <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+                <RevenueChart revenue={revenue} />
+            </div>
+        </main>
+    )
 }
