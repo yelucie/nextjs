0a0691bd96e49db9dc308778c5c62dd3bfb53aee
Lucie <lye10@hawk.iit.edu>
2024-06-04 02:11:09 -0500

    Added latest invoices. Data not fetching

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '../ui/dashboard/revenue-chart';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import { lusitana } from '../ui/fonts';
-import { fetchRevenue } from '@/app/lib/data';
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
 
 export default async function DashboardPage() {
     const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
 
     return (
         <main>
@@ -13,6 +14,7 @@ export default async function DashboardPage() {
             <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'></div>
             <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
                 <RevenueChart revenue={revenue} />
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     )
