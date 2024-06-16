1f967a070e83141e534f8201d72305d4fbd1d109
Lucie <lye10@hawk.iit.edu>
2024-06-16 00:46:12 -0500

    Added error pages

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,17 +6,26 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
-export default function InvoicesPage() {
+export default function InvoicesPage({ searchParams }: { searchParams?: {
+    query?: string;
+    page?: string;
+}}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page || 1);
+
   return (
     <div className="w-full">
-      <div className="flex w-full items-center justify-between">
+        <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
-      </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search invoices..." />
-        <CreateInvoice />
-      </div>
-      <div className="mt-5 flex w-full justify-center"></div>
+        </div>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+            <Search placeholder="Search invoices..." />
+            <CreateInvoice />
+        </div>
+        <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+            <Table query={query} currentPage={currentPage}/>
+        </Suspense>
+        <div className="mt-5 flex w-full justify-center"></div>
     </div>
   );
 }
